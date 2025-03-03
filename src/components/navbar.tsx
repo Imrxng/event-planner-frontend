3128669f66faad0d5740ff916df82c8637600381
snoopychr <ayoub21cherif@hotmail.com>
2025-03-03 10:22:19 +0100

    m

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,3 @@
-
 import { AiOutlineRight } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/brightest_logo_black_yellow.png";
@@ -6,30 +5,42 @@ import "../styles/navbar.component.css";
 import { useState } from "react";
 
 export default function Navbar() {
-  const [logged,setLogged] = useState(false);
+  const [logged, setLogged] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
   return (
     <>
-    {logged ?
       <nav>
         <Link to="/" className="bright-logo">
           <img src={logo} alt="bright-logo" />
         </Link>
         <div className="nav-links">
-              <p className="nav-login">log in</p>
-              <AiOutlineRight className="nav-icon"/>
+          <p className="nav-login" onClick={toggleDropdown}>
+            {logged ? "Profile" : "Log in"}
+          </p>
+          <AiOutlineRight className="nav-icon" />
+          {dropdownOpen && (
+            <div className="dropdown-menu">
+              {logged ? (
+                <>
+                  <Link to="/profile">Profile</Link>
+                  <Link to="/settings">Settings</Link>
+                  <Link to="/logout">Log out</Link>
+                </>
+              ) : (
+                <>
+                  <Link to="/login">Log in</Link>
+                  <Link to="/register">Register</Link>
+                </>
+              )}
+            </div>
+          )}
         </div>
-      </nav>:
-      <nav>
-        <Link to="/login" className="bright-logo">
-          <img src={logo} alt="bright-logo" />
-        </Link>
-        <div  className="nav-links">
-              <p className="nav-login">Login</p>
-              <AiOutlineRight className="nav-icon"/>
-        </div>
-        </nav>
-      }
+      </nav>
     </>
-  )
+  );
 }
